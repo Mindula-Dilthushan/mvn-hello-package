3351bd62ef1163f1538199e344e2cbce3399c549
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-10 12:58:37 +0530

    reset

--- src/main/java/RedAlpha.java
+++ src/main/java/RedAlpha.java
@@ -4,7 +4,7 @@
  * 22-03-10
  */
 public class RedAlpha {
-    public static void MyIGN(String ign){
+    public static void display(String ign){
         System.out.println("Hello There, My IGN is "+ign);
     }
 }
