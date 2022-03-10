e142df3927013a98908de6184f1c421fa2b5cf1b
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-03-10 12:37:17 +0530

    class-create

---
+++ src/main/java/RedAlpha.java
@@ -0,0 +1,10 @@
+/**
+ * Mindula Dilthushan
+ * Maven Hello Package
+ * 22-03-10
+ */
+public class RedAlpha {
+    public static void display(String ign){
+        System.out.println("Hello There, My IGN is "+ign);
+    }
+}
